7960f889d813a012d88ed8c3821cff162c80cb6d
iamxloss <47696079+iamxloss@users.noreply.github.com>
2019-04-09 20:40:43 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+var CONFIG = {
+	"nightbotToken" : "4abb614c",
+	"googleApiKeys" : ["AIzaSyC7asPTom1oZVPmZu7UcttGNFvqzRWQiRM", "AIzaSyDUUfmvtaHY3lQ11CbkF8gplSJSXwgLe2g", "AIzaSyAExj-s-xE2tEkhVvypLD2Lg0AuuVRecf0", "AIzaSyD-FZf0t0cS0zXLulBo-Ybkfb-4bQeXw-w", "AIzaSyBxwV-Gc9dQt58-E38BKiQcLExi9nYw5gc", "AIzaSyAoY-Wp6oKkD_BEYSmnGDnMP0oX23RXBnI", "AIzaSyCV2bGlFpE4Wh8OpZjPNgwUx5IBMQJ-llQ", "AIzaSyBKNdqfqDMJmOXb7fuIeICDlRGiXpsDqLg", "AIzaSyCiJaKnEWOFfwlQHJZWhrMCRvG8qW-Z7JM", "AIzaSyAc0dURA4OX1tM3lutoJ7z3LFvTrNS7u9Q"],
+	"numberOfChannels" : 18,
+	"numberOfRows" : 3
+};
